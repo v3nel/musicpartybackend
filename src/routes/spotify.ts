b658b83fe93b4b8c6a1432fb1c81c31dc5be714e
unknown <erazzorlinx@gmail.com>
2026-05-03 16:45:59 +0200

    feat: add Spotify login route

---
+++ src/routes/spotify.ts
@@ -0,0 +1,29 @@
+import { Router, Request, Response } from "express"
+
+export const spotifyRouter = Router()
+
+spotifyRouter.get("/login/", async (req: Request, res: Response) => {
+    const client_id = process.env.SPOTIFY_CLIENT_ID
+    const redirect_uri = process.env.SPOTIFY_REDIRECT_URI
+    const scopes = process.env.SPOTIFY_SCOPES
+
+    try {
+        if (!client_id || !redirect_uri || !scopes) {
+            throw new Error("client_id or redirect_uri or scopes for spotify are not specified in environment variables")
+        }
+        
+        const url = new URL("https://accounts.spotify.com/authorize")
+        url.searchParams.set("client_id", client_id)
+        url.searchParams.set("response_type", "code")
+        url.searchParams.set("redirect_uri", redirect_uri)
+        url.searchParams.set("show_dialog", "true")
+        url.searchParams.set("scopes", scopes)
+
+        res.redirect(url.toString())
+    } catch(e) {
+        res.send({
+            code: 501,
+            message: (e instanceof Error) ? e.message : "An unknown error occurred"
+        })
+    }
+})
