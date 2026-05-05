0928f9a76026952c934aa6f6cc6074d825e426c0
v3nel <me@v3nel.dev>
2026-05-05 21:20:20 +0200

    fix: correct Spotify router scope handling

--- src/routes/spotify.ts
+++ src/routes/spotify.ts
@@ -5,11 +5,11 @@ export const spotifyRouter = Router()
 spotifyRouter.get("/login/", async (req: Request, res: Response) => {
     const client_id = process.env.SPOTIFY_CLIENT_ID
     const redirect_uri = process.env.SPOTIFY_REDIRECT_URI
-    const scopes = process.env.SPOTIFY_SCOPES
+    const scope = process.env.SPOTIFY_SCOPES
 
     try {
-        if (!client_id || !redirect_uri || !scopes) {
-            throw new Error("client_id or redirect_uri or scopes for spotify are not specified in environment variables")
+        if (!client_id || !redirect_uri || !scope) {
+            throw new Error("client_id or redirect_uri or scope for spotify are not specified in environment variables")
         }
         
         const url = new URL("https://accounts.spotify.com/authorize")
@@ -17,12 +17,12 @@ spotifyRouter.get("/login/", async (req: Request, res: Response) => {
         url.searchParams.set("response_type", "code")
         url.searchParams.set("redirect_uri", redirect_uri)
         url.searchParams.set("show_dialog", "true")
-        url.searchParams.set("scopes", scopes)
+        url.searchParams.set("scope", scope)
 
         res.redirect(url.toString())
     } catch(e) {
-        res.send({
-            code: 501,
+        res.status(500).json({
+            code: 500,
             message: (e instanceof Error) ? e.message : "An unknown error occurred"
         })
     }
